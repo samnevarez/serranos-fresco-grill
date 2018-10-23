b180f0c486dd7c84535ef45d28a08bef7e08ee09
Sammuel Nevarez <snevarez89@gmail.com>
2018-10-22 22:51:45 -0700

    cleaned up some html and optimized images

---
+++ public/js/serranos-js.js
@@ -0,0 +1,156 @@
+// jQuery Slide Toggle
+( function($) {
+
+// Nachos Grandes Toggles
+    $(document).ready(function() {
+        $('#nachosgrandesShow').click(function(){
+	        $('#nachosgrandesShow').slideToggle();
+	        $('#nachosgrandesHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#nachosgrandesHide').click(function(){
+        	$('#nachosgrandesHide').slideToggle();
+        	$('#nachosgrandesShow').slideToggle();
+        });
+    })
+
+// Burrito Toggles
+    $(document).ready(function() {
+        $('#burritoShow').click(function(){
+	        $('#burritoShow').slideToggle();
+	        $('#burritoHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#burritoHide').click(function(){
+        	$('#burritoHide').slideToggle();
+        	$('#burritoShow').slideToggle();
+        });
+    })
+
+// Soft Taco Toggles
+    $(document).ready(function() {
+        $('#softtacoShow').click(function(){
+	        $('#softtacoShow').slideToggle();
+	        $('#softtacoHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#softtacoHide').click(function(){
+        	$('#softtacoHide').slideToggle();
+        	$('#softtacoShow').slideToggle();
+        });
+    })
+
+// Tostada Toggles
+    $(document).ready(function() {
+    	$('#tostadaShow').click(function() {
+			$('#tostadaShow').slideToggle();
+    		$('#tostadaHide').slideToggle();
+    	});
+    })
+    $(document).ready(function() {
+        $('#tostadaHide').click(function(){
+        	$('#tostadaHide').slideToggle();
+        	$('#tostadaShow').slideToggle();
+        });
+
+// Quesadilla Toggles
+    $(document).ready(function() {
+        $('#quesadillaShow').click(function(){
+	        $('#quesadillaShow').slideToggle();
+	        $('#quesadillaHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#quesadillaHide').click(function(){
+        	$('#quesadillaHide').slideToggle();
+        	$('#quesadillaShow').slideToggle();
+        });
+    })
+
+// Tortas Toggles
+    $(document).ready(function() {
+        $('#tortasShow').click(function(){
+	        $('#tortasShow').slideToggle();
+	        $('#tortasHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#tortasHide').click(function(){
+        	$('#tortasHide').slideToggle();
+        	$('#tortasShow').slideToggle();
+        });
+    })
+
+// A La Carte Toggles
+    $(document).ready(function() {
+        $('#alacarteShow').click(function(){
+	        $('#alacarteShow').slideToggle();
+	        $('#alacarteHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#alacarteHide').click(function(){
+        	$('#alacarteHide').slideToggle();
+        	$('#alacarteShow').slideToggle();
+        });
+    })
+
+// Combination Plate Toggles
+    $(document).ready(function() {
+        $('#combinationShow').click(function(){
+	        $('#combinationShow').slideToggle();
+	        $('#combinationHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#combinationHide').click(function(){
+        	$('#combinationHide').slideToggle();
+        	$('#combinationShow').slideToggle();
+        });
+    })
+
+// Lighter Side Toggles
+    $(document).ready(function() {
+        $('#lightersideShow').click(function(){
+	        $('#lightersideShow').slideToggle();
+	        $('#lightersideHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#lightersideHide').click(function(){
+        	$('#lightersideHide').slideToggle();
+        	$('#lightersideShow').slideToggle();
+        });
+    })
+
+// Breakfast Plates Toggles
+    $(document).ready(function() {
+        $('#breakfastPlatesShow').click(function(){
+            $('#breakfastPlatesShow').slideToggle();
+            $('#breakfastPlatesHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#breakfastPlatesHide').click(function(){
+            $('#breakfastPlatesHide').slideToggle();
+            $('#breakfastPlatesShow').slideToggle();
+        });
+    })
+
+// Breakfast Burrito Toggles
+    $(document).ready(function() {
+        $('#breakfastBurritoShow').click(function(){
+	        $('#breakfastBurritoShow').slideToggle();
+	        $('#breakfastBurritoHide').slideToggle();
+        });
+    })
+    $(document).ready(function() {
+        $('#breakfastBurritoHide').click(function(){
+        	$('#breakfastBurritoHide').slideToggle();
+        	$('#breakfastBurritoShow').slideToggle();
+        });
+    })
+    })})( jQuery );
